04122d321e5bb010dbcf4dfc1588eb404e65608f
alfe_below <alfe10251@gmail.com>
2016-10-24 19:54:17 +0900

    make base

---
+++ views/main.jsx
@@ -0,0 +1,14 @@
+'use babel';
+
+import React from 'react';
+
+export default class Main extends React.Component {
+  render() {
+    return(
+    <main>
+      <div>Hello from React with ES6 :)</div>
+      <img src='img/sakura.png'></img>;
+    </main>
+    )
+  }
+}
