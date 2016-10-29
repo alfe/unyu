05dc518dfcbceb9ad0a7af8c923d969a9c7d5b56
alfe_below <alfe10251@gmail.com>
2016-10-29 11:23:22 +0900

    attach css to comment

--- views/comment.jsx
+++ views/comment.jsx
@@ -5,7 +5,14 @@ import React from 'react';
 export default class Comment extends React.Component {
   render() {
     return(
-      <p>Electronで伺か作るよ!</p>
+      <p className='comment' style={{
+          border:'medium dotted #999',
+          background:'#FFFFFF',
+          padding:'10px',
+          width : '200px',
+        }}>
+        Electronで伺か作るよ!
+      </p>
     )
   }
 }
