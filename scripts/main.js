75722ef2ef29cd077a34cdc5a471c18a6341327b
alfe_below <alfe10251@gmail.com>
2016-11-10 07:04:12 +0900

    add onClick

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ import Character from '../views/character.jsx';
 window.onload = function(){
   ReactDOM.render(<Main />, document.body);
   ReactDOM.render(<Comment name="伺かってこんなんだっけ…?" />, document.getElementById('comment_sakura') );
-  ReactDOM.render(<Comment name="まぁ、似せてるだけしなぁ" />, document.getElementById('comment_unyu') );
+  ReactDOM.render(<Comment name="まぁ、似せてるだけやしなぁ" />, document.getElementById('comment_unyu') );
   ReactDOM.render(<Character character="sakura" />, document.getElementById('character_sakura') );
   ReactDOM.render(<Character character="unyu" />, document.getElementById('character_unyu') );
 }
--- views/comment.jsx
+++ views/comment.jsx
@@ -3,9 +3,12 @@
 import React from 'react';
 
 export default class Comment extends React.Component {
+  onClick(e) {
+    console.log(this);
+  };
   render() {
     return(
-      <p className='comment'>
+      <p className='comment' onClick={this.onClick}>
         {this.props.name}
       </p>
     )
